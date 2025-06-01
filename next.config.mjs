2b85e27d8f3c6b3c92e7db8fe6daa9cd73c0d832
drewthekiiid <drew@drewthekiiid.com>
2025-06-01 18:56:37 -0500

    Fix static export build by adding trailingSlash option

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? "/gsha2025" : "",
   assetPrefix: process.env.NODE_ENV === 'production' ? "/gsha2025" : "",
   output: "export",
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
